a6198263a9b286a9b88d036061e5c5c4ccdf8d06
Jzn <133604852+Jezuun@users.noreply.github.com>
2026-01-20 16:17:47 -0500

    Add comments for clarity in Main class
    Added comments to clarify deposit, withdrawal, and equals method usage.

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     account acc1 = new account(420, "jesse" , 8000);
     account acc2 = new account (78, "ren", 20);
 
-
+   // calling deposit
     acc2.deposit(2000);
     acc1.withdraw(500);
 
@@ -14,10 +14,11 @@ public class Main {
     System.out.println(acc2);
 
     System.out.println("Total number of accounts created : " + account.getNumberOfAccounts());
-
+//calling equals and allowing it to print wheather true or false
     System.out.println(acc1.equals(acc2));
     }
 
 }
+
 
 
